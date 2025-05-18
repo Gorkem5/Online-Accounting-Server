4aa7105d6499c9ff68190a9df2fa80403794ac9a
Gorkem <103636907+Gorkem5@users.noreply.github.com>
2025-05-18 15:08:44 +0300

    Add project files.

---
+++ OnlineMuhasebeServer.Presentation/Abstraction/ApiContoller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMuhasebeServer.Presentation.Abstraction
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class ApiContoller : ControllerBase
+    {
+    }
+}
---
+++ OnlineMuhasebeServer.Presentation/Controller/DemoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineMuhasebeServer.Presentation.Abstraction;
+
+namespace OnlineMuhasebeServer.Presentation.Controller
+{
+    public sealed class DemoController : ApiContoller
+    {
+        [HttpGet]
+        public IActionResult GetDemo()
+        {
+            return Ok("Demo endpoint is working!");
+        }
+
+    }
+}
